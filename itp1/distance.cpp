8e6c4585c9f2202bbee2b9175da6ff3f4eec70e2
hugo-maker <hibiki.for.business1@gmail.com>
2021-05-07 16:28:23 +0900

    Added today's activities

---
+++ itp1/distance.cpp
@@ -0,0 +1,100 @@
+#include <cmath>
+#include <iostream>
+#include <vector>
+#include <iomanip>
+#include <algorithm>
+using namespace std;
+#define EPS (1e-10)
+#define equals(a, b) (fabs((a) - (b)) < EPS)
+
+class Point
+{
+public:
+  double x, y;
+
+  Point(double x = 0, double y = 0)
+    : x(x), y(y) {}
+
+  Point operator + (Point p)
+  {
+    return Point(x + p.x, y + p.y);
+  }
+  Point operator - (Point p)
+  {
+    return Point(x - p.x, y - p.y);
+  }
+  Point operator * (double a)
+  {
+    return Point(a * x, a * y);
+  }
+  Point operator / (double a)
+  {
+    return Point(a / x, a / y);
+  }
+
+  double abs()
+  {
+    return sqrt(norm());
+  }
+  double norm()
+  {
+    return x * x + y * y;
+  }
+
+  bool operator < (const Point & p) const
+  {
+    return x != p.x ? x < p.x : y < p.y;
+  }
+
+  bool operator == (const Point & p) const
+  {
+    return fabs(x - p.x) < EPS && fabs(y - p.y) < EPS;
+  }
+};
+
+typedef Point Vector;
+
+double norm(Vector a)
+{
+  return a.x * a.x + a.y * a.y;
+}
+
+double abs(Vector a)
+{
+  return sqrt(norm(a));
+}
+
+struct Segment
+{
+  Point p1, p2;
+};
+
+typedef Segment Line;
+
+double dot(Vector a, Vector b)
+{
+  return a.x * b.x + a.y * b.y;
+}
+
+double cross(Vector a, Vector b)
+{
+  return a.x * b.y - a.y * b.x;
+}
+
+double get_distance(Point a, Point b)
+{
+  return abs(a - b);
+}
+
+int main()
+{
+  double x1, y1, x2, y2;
+  cin >> x1 >> y1 >> x2 >> y2;
+
+  Point P1(x1, y1);
+  Point P2(x2, y2);
+
+  cout << setprecision(20) << get_distance(P1, P2) << endl;
+
+  return 0;
+}
